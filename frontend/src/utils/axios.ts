8fcafae4feaa8a7b38d2ffa57254d53008ecf6fb
Bohdan <60944532+BohdanNikolaychuk@users.noreply.github.com>
2023-03-15 13:01:59 +0200

    Update axios.ts

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instans = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://storeapp-production-8870.up.railway.app/'
 });
 
 instans.interceptors.request.use(async (config) => {
